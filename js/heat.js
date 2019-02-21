3f785a94b7b80d5bc0f05270812b4187b7316dc9
Scott Garner <scott@j38.net>
2019-02-20 16:58:23 -0800

    Change to parameter from front-end config.

--- js/heat.js
+++ js/heat.js
@@ -4,13 +4,13 @@ class Heat extends EventTarget {
         super();
 
         const params = (new URL(document.location)).searchParams;
-        const channel = params.get("channel") || 97032862;
+        const channel = params.get("channelID") || 97032862;
 
         const url = 'wss://heat-ebs.j38.net/';
         //const url = 'ws://localhost:3000/';
         const socket = io(url, { transports: ['websocket'] });
 
-        console.log("Heat for channel: " + channel + " " + url);
+        console.log("Running Heat for channel: " + channel + " " + url);
 
         //
 
